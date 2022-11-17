7b45eb94c705e204d156f6453a5a23d8a51c8017
kael-cadranel <kael.cadranel00@gmail.com>
2022-11-17 12:06:22 +0100

    commit fini-changementdecouleur

--- starter/04-change-couleur-1/script.js
+++ starter/04-change-couleur-1/script.js
@@ -6,7 +6,14 @@ EXERCICE 4 : Change couleur (1)
 Au clic sur un des boutons, colorie la page dans la couleur choisie par l'utilisateur.
 */
 
+const buttons = document.querySelectorAll('.actions button');
 
+for (const button of buttons) {
+    button.addEventListener('click', (event) => {
+            document.body.style.background = event.currentTarget.id;
+        }
+    )
+}
 
 
 
